2c063fa54937d79362d207371fdde98533e4ee29
Hussein-Eissa <Hussein.ShabanIS21@fcai.usc.edu.eg>
2025-05-09 04:37:03 +0300

    Edit API Files

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const connectDB = require('./config/db');
 const taskRoutes = require('./routes/taskRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to handle CORS
-const cors = require('cors');
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -15,6 +15,7 @@ app.use(cors({
 }));
 
 connectDB();
+app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 app.use('/api/categories', categoryRoutes);
